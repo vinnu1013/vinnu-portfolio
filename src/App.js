abe31700c8bc9c1816003ff0d0b5d3dd5c543f11
Manohar4333 <thappetlamanohar@gmail.com>
2024-08-10 15:26:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import './index.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <link rel="stylesheet" href="./style.css"></link>
+    <div className="Hero">
+        <nav>
+        <ul>
+            <li><a href="./Cv.html">Home</a></li>
+            <li><a href="./Education.html">Education</a></li>
+            <li><a href="./Prijects.html">Projects</a></li>
+            <li><a href="./certificates.html">Certificates</a></li>
+            <li><a href="./About.html">About</a></li>
+        </ul>
+    </nav>
     </div>
+    <div className="Detail">
+        <h1>Hello World 👋<span> I'm Vinay Thappetla </span></h1>
+        <p>Student of Mallareddy College Of Engineering And Technology</p>
+        <p>With Stream Internet Of Things</p>
+        <a href="Vinay Resume.pdf">DOWNLOAD CV</a>
+    </div>
+    <div className="image">
+        <img src="./Holding Laptop.png" alt="" className="holding-laptop"/>
+    </div>
+    
+</div>
   );
 }
 
