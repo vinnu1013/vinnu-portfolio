6393429141b2266da81c1dad6afd6a630eb4ddf5
Manohar4333 <thappetlamanohar@gmail.com>
2024-08-11 22:27:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
     <div className="Hero">
         <nav>
         <ul>
-            <li><a href="./Cv.html">Home</a></li>
-            <li><a href="./Education.html">Education</a></li>
-            <li><a href="./Prijects.html">Projects</a></li>
-            <li><a href="./certificates.html">Certificates</a></li>
-            <li><a href="./About.html">About</a></li>
+            <li><i><a href="./Cv.html">Home</a></i></li>
+            <li><i><a href="./Education.html">Education</a></i></li>
+            <li><i><a href="./Projects.html">Projects</a></i></li>
+            <li><i><a href="./certificates.html">Certificates</a></i></li>
+            <li><i><a href="./About.html">About</a></i></li>
         </ul>
     </nav>
     </div>
@@ -20,12 +20,26 @@ function App() {
         <h1>Hello World 👋<span> I'm Vinay Thappetla </span></h1>
         <p>Student of Mallareddy College Of Engineering And Technology</p>
         <p>With Stream Internet Of Things</p>
-        <a href="Vinay Resume.pdf">DOWNLOAD CV</a>
+        <a className='Download-cv' href="Certificates.pdf">DOWNLOAD CV</a>
     </div>
     <div className="image">
         <img src="./Holding Laptop.png" alt="" className="holding-laptop"/>
     </div>
-    
+    <div className="footer">
+    <footer>
+    <div class="footerconainer">
+        <div class="socialicons">
+             <a href="https://instagram.com/_mr.vinnu10_?igshid=MzNlNGNkZWQ4Mg==" target="_blank"><ion-icon name="logo-instagram" ></ion-icon></a>
+             <a href="https://twitter.com/v_innu?t=EYqADrU3DW2klTk-BaG0Fw&s=08" target="_blank"><ion-icon name="logo-twitter"></ion-icon></a>
+            <a href="https://www.linkedin.com/in/thappetla-vinay-kumar-88778b1bb" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
+            <a href="wa.me/919059236500" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon></a>
+        </div>
+    </div>
+    <div class="footerbottom">
+        <p>Copyright &copy;2022 Designed By <span class="designer">Thappetla Vinay Kumar</span></p>
+    </div>
+</footer>
+    </div>
 </div>
   );
 }
